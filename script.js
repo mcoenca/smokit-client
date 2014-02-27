92cd70fcf77828d99aa620bee7d2c400a94a45f5
mcoenca <m.coenca@gmail.com>
2014-02-27 18:43:53 +0100

    Ca marche avec css et connection server mais fichiers en trop à supprimer

--- script.js
+++ script.js
@@ -1,144 +1,48 @@
-//Attend le chargement du DOM
-$(function(){
-	//Réglage de la page au début : déclaration variable globale et cachage de connecté et stats
-	var username='';
-	$(".connected").hide();
-	$(".stats").hide();
+//Attend le chargement du DOM que ce soit en Ruby (page load)
+var username='';
 
-	//Quand on clique sur le bouton d'id 'Connect'
+//Execute après le chargement du DOM
+var ready = function(){
+
+//Accueil
+	$(".disconnected").show();
+
+//Réglage de ce qu'on accepte comme input grace au plugin jquery alphanum(). Evite création d'utilisateurs aux noms mauvais pour les requetes. A COMPLETER DANS LE BACKEND
+	$(".restricted_input").alphanum();
+
+//Quand on clique sur le bouton d'id 'Connect'
 	$('#connect').click(function() {
-		//On prend la variable username dans l'input "Connect"
-		username=$('#username_connect').val();
-		//on fait la requete GET correspondante
-		$.ajax({
-			url: "http://holdit.herokuapp.com/users/name/"+username+".json",
-			dataType: "json",
-			type: "get",
-			headers: { 
-        			Accept : "application/json"
-   				 },
-			success: function(donnees){
-				//Connection réussie ! Ici on va écrire les modifications pour passer de non connecté à connecté dans le DOM
-
-				$(".connected").show();
-				$(".disconnected").hide();
-				$("#title_smoke").text("Smok'it, "+username+" !");
-				//var l = donnees['smokes'].length;
-				//console.log(l);
-			},
-			error: function(xhr,textStatus,errorThrown){
-				//Connection ratée... On rajoute l'erreur dans le DOM
-				console.log(textStatus);
-				console.log(errorThrown);
-				$('#error1').show();
-				$('#username_connect').css("border-color","red");
-			}
-		});
-
+		Connect();
 	});
 
-	//Quand on clique sur le bouton d'id 'Nouvel utilisateur'
+//Quand on clique sur le bouton d'id 'Nouvel utilisateur'
 	$('#create').click(function() {
-		//On prend la variable username dans l'input de texte "Nouvel util"
-		username=$('#username_create').val();
-		//on fait la requete POST correspondante
-		$.ajax({
-			url: "http://holdit.herokuapp.com/users",
-			type: "post",
-			dataType: "json",
-			headers: { 
-        			Accept : "application/json"
-   				 },
-			data: {"authenticity_token":"WE9L/lhK8otgTy/+UZd8jOjGYBnRMs2I37JUL3v3tjQ=", "user[name]":username},
-			success: function(donnees){
-				//Création réussie ! Ici on va écrire les modifications pour passer de non connecté à connecté dans le DOM
-				$(".connected").show();
-				$(".disconnected").hide();
-				$("#title_smoke").text("Smok'it, "+username+" !");
-			},
-			error: function(xhr,textStatus,errorThrown){
-				//Connection ratée... On rajoute l'erreur dans le DOM
-				console.log(textStatus);
-				console.log(errorThrown);
-				$('#error2').show();
-				$('#username_create').css("border-color","red");
-			}
-		});
-
+		Create();
 	});
 
-	//Quand on clique sur le bouton d'id 'NEW SMOKE'
+//Quand on clique sur le bouton d'id 'Nouvelle smoke'
 	$('#smoke').click(function() {
-		//On prend la variable username la où elle est ! C'est à dire déja bien réglée
-		//on fait la requete POST correspondante
-		$.ajax({
-			url: "http://holdit.herokuapp.com/newsmoke",
-			type: "post",
-			dataType: "json",
-   			headers: { 
-        			Accept : "application/json"
-   				 },
-			data: {"authenticity_token":"WE9L/lhK8otgTy/+UZd8jOjGYBnRMs2I37JUL3v3tjQ=",
- 				"user[name]":username,
-				"smoke[smoke_latitude]":1,
-				"smoke[smoke_longitude]":2,
-				"smoke[smoke_date]":new Date()
-			},
-			success: function(donnees){
-				//Smoke réussie ! On affiche l'animation pour le montrer au monsieur
-			alert('Smoke créée... à :'+ new Date());
-			},
-			error: function(xhr,textStatus,errorThrown){
-				//Connection ratée... On affiche une animation pour le montrer au monsieur
-				//console.log(textStatus);
-				//console.log(errorThrown);
-			alert("La connection au server n'a pas réussi, c'est dommage");
-			}
-		});
-
+		Smoke();
 	});
 
-	//Quand on clique sur le bouton go_smoke -> stats
+//Quand on clique sur le bouton go_smoke -> stats
 	$('#go_smoke_stats').click(function() {
-	//Remet à zéro les stats et affiche l'écran
-		$(".smoke_list").remove();
-		$(".connected").hide();
-		$(".stats").show();
-		$("#title_stats").text("Tes stats, "+username+" !");
-	//requete ajax pour  remplir le tableau joli !
-		$.ajax({
-		url: "http://holdit.herokuapp.com/users/name/"+username+".json",
-		dataType: "json",
-		headers: { 
-        		Accept : "application/json"
-   			 },
-		type: "get",
-		success: function(donnees){
-			//Ca a marché, on va afficher bien les données dans le DOM .stats
-			var l = donnees['smokes'].length;
-			console.log(l);
-			//Parcours à refaire pour une meilleure présentation
-			$.each(donnees['smokes'],function(){
-				$("#stat_table").append("<tr class='smoke_list'><th>"+this.smoke_date+
-				"</th><th>"+this.smoke_latitude+
-				"</th><th>"+this.smoke_longitude+
-				"</th></tr>");
-			});
-
-		},
-		error: function(xhr,textStatus,errorThrown){
-			//Ca a raté, message d'erreur
-			console.log(textStatus);
-			console.log(errorThrown);
-			alert('impossible de récupérer les stats...');
-			}
-		});
-
+		Stats();
 	});
-	//Quand on clique sur le bouton go_stats->smoke
-	$('#go_stats_smoke').click(function() {
-		$(".connected").show();
-		$(".stats").hide();
-		$("#title_stats").text("Tes stats, "+username+" !");
+
+//Quand on clique sur le bouton go_stats->smoke
+	$('#go_stats_smoke').click(function(){
+		go_stats_smoke();
 	});
-});
+
+//Quand on clique sur le bouton go_smoke->home
+	$('#go_smoke_home').click(function() {
+		go_smoke_home(); 
+	});
+};
+
+//Vérifie si le reload de la page provient de rails, ou est normal, pour bien gérer le reload du javascript sur la page home
+$(document).ready(ready);
+$(document).on('page:load', ready);
+
+
