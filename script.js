bc1f21f542c695cfdacaebb3914cb7b70994d24c
mcoenca <m.coenca@gmail.com>
2014-02-28 00:33:01 +0100

    test geoloc

--- script.js
+++ script.js
@@ -4,6 +4,37 @@ var username='';
 //Execute après le chargement du DOM
 var ready = function(){
 
+alert('page prete');
+document.addEventListener("deviceready", onDeviceReady, false);
+
+// Cordova is ready
+//
+function onDeviceReady() {
+var options = { timeout: 5000 };
+navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
+}
+
+// onSuccess Geolocation
+//
+function onSuccess(position) {
+var element = document.getElementById('geolocation');
+element.innerHTML = 'Latitude: ' + position.coords.latitude + '<br />' +
+'Longitude: ' + position.coords.longitude + '<br />' +
+'Altitude: ' + position.coords.altitude + '<br />' +
+'Accuracy: ' + position.coords.accuracy + '<br />' +
+'Altitude Accuracy: ' + position.coords.altitudeAccuracy + '<br />' +
+'Heading: ' + position.coords.heading + '<br />' +
+'Speed: ' + position.coords.speed + '<br />' +
+'Timestamp: ' +
+position.timestamp + '<br />';
+}
+
+// onError Callback receives a PositionError object
+//
+function onError(error) {
+alert('code: ' + error.code + '\n' +
+'message: ' + error.message + '\n');
+}
 //Accueil
 	$(".disconnected").show();
 
