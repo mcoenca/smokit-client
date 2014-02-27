7bf354dbd2935c3d3a104fe195720417e3498703
mcoenca <m.coenca@gmail.com>
2014-02-28 00:35:47 +0100

    test geoloc

--- script.js
+++ script.js
@@ -10,23 +10,14 @@ document.addEventListener("deviceready", onDeviceReady, false);
 // Cordova is ready
 //
 function onDeviceReady() {
-var options = { timeout: 5000 };
+var options = { timeout: 31000, enableHighAccuracy: true, maximumAge: 90000 };
 navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
 }
 
 // onSuccess Geolocation
 //
 function onSuccess(position) {
-var element = document.getElementById('geolocation');
-element.innerHTML = 'Latitude: ' + position.coords.latitude + '<br />' +
-'Longitude: ' + position.coords.longitude + '<br />' +
-'Altitude: ' + position.coords.altitude + '<br />' +
-'Accuracy: ' + position.coords.accuracy + '<br />' +
-'Altitude Accuracy: ' + position.coords.altitudeAccuracy + '<br />' +
-'Heading: ' + position.coords.heading + '<br />' +
-'Speed: ' + position.coords.speed + '<br />' +
-'Timestamp: ' +
-position.timestamp + '<br />';
+alert('success');
 }
 
 // onError Callback receives a PositionError object
