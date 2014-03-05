b90cdf73b65844a4aeca3c769474710ee06ae92b
mcoenca <m.coenca@gmail.com>
2014-03-06 00:29:07 +0100

    test Phonegap1

--- connections.js
+++ connections.js
@@ -5,25 +5,6 @@
 var Connect=function($deja_connect) {
 	//On prend la variable username dans l'input "Connect" si on est pas déja connecté et on le met dans localStorage
 
-var onSuccess = function(position) {
-    alert('Latitude: '          + position.coords.latitude          + '\n' +
-          'Longitude: '         + position.coords.longitude         + '\n' +
-          'Altitude: '          + position.coords.altitude          + '\n' +
-          'Accuracy: '          + position.coords.accuracy          + '\n' +
-          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-          'Heading: '           + position.coords.heading           + '\n' +
-          'Speed: '             + position.coords.speed             + '\n' +
-          'Timestamp: '         + position.timestamp                + '\n');
-};
-
-// onError Callback receives a PositionError object
-//
-function onError(error) {
-    alert('code: '    + error.code    + '\n' +
-          'message: ' + error.message + '\n');
-}
-
-navigator.geolocation.getCurrentPosition(onSuccess, onError);
 	if($deja_connect==false){
 		username=$('#username_connect').val();
 		localStorage.setItem('username',username);
